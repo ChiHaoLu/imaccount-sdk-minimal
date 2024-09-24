4ed2e9dd30cefba67eac25d8adcbc00413ba939a
ChiHaoLu <b08303113@ntu.edu.tw>
2024-09-24 16:30:19 +0800

    improve front end

--- packages/front-end/pages/index.tsx
+++ packages/front-end/pages/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import * as sdk from "@consenlabs/imaccount-sdk";
+import {
+  ADDRESS,
+  AccountEntryFactory,
+  imAccount,
+  BundlerProvider,
+  BundlerMode,
+} from "@consenlabs/imaccount-sdk";
 import { ethers, Eip1193Provider, BrowserProvider } from "ethers";
 import { JsonRpcSignerECDSAValidator } from "./JsonRpcSignerECDSAValidator";
 
@@ -11,67 +17,82 @@ declare global {
 
 const Home = () => {
   const [accountInfo, setAccountInfo] = useState<{
-    account: sdk.imAccount;
+    account: imAccount;
     isDeployed: boolean;
   } | null>(null);
   const [chainID, setChainID] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [initLoading, setInitLoading] = useState(false);
+  const [bundlerRpcUrl, setBundlerRpcUrl] = useState(
+    (process.env.NEXT_PUBLIC_RPC_URL as string) ?? ""
+  );
+  const [userSalt, setUserSalt] = useState(123);
 
-  useEffect(() => {
-    const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL as string;
-    const ENTRY_POINT_ADDRESS = sdk.ADDRESS.ENTRY_POINT;
-    const ECDSA_VALIDATOR_ADDRESS = sdk.ADDRESS.SEPOLIA.ecdsaValidator;
-    const ACCOUNT_FACTORY_ADDRESS = sdk.ADDRESS.SEPOLIA.imAccountFactory;
-    const ENTRY_FACTORY_ADDRESS = sdk.ADDRESS.SEPOLIA.accountEntryProxyFactory;
-    const USER_SALT = 123n;
+  const ENTRY_POINT_ADDRESS = ADDRESS.ENTRY_POINT;
+  const ECDSA_VALIDATOR_ADDRESS = ADDRESS.SEPOLIA.ecdsaValidator;
+  const ACCOUNT_FACTORY_ADDRESS = ADDRESS.SEPOLIA.imAccountFactory;
+  const ENTRY_FACTORY_ADDRESS = ADDRESS.SEPOLIA.accountEntryProxyFactory;
 
-    const fetchData = async () => {
-      const bundler = new sdk.BundlerProvider({
-        url: RPC_URL,
-        entryPoint: ENTRY_POINT_ADDRESS,
-        mode: sdk.BundlerMode.Auto,
+  const fetchData = async () => {
+    const bundler = new BundlerProvider({
+      url: bundlerRpcUrl,
+      entryPoint: ENTRY_POINT_ADDRESS,
+      mode: BundlerMode.Auto,
+    });
+    setChainID(Number(await bundler.getChainId()));
+  };
+
+  const initAccount = async () => {
+    setInitLoading(true);
+    const userSaltBigInt = BigInt(userSalt);
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const bundler = new BundlerProvider({
+      url: bundlerRpcUrl,
+      entryPoint: ENTRY_POINT_ADDRESS,
+      mode: BundlerMode.Auto,
+    });
+    const validator = new JsonRpcSignerECDSAValidator({
+      rpcUrl: bundlerRpcUrl,
+      address: ECDSA_VALIDATOR_ADDRESS,
+      owner: await provider.getSigner(),
+    });
+    try {
+      const account = await imAccount.create({
+        rpcUrl: bundlerRpcUrl,
+        bundler: bundler,
+        imAccountFactoryAddress: ACCOUNT_FACTORY_ADDRESS,
+        accountEntryFactoryAddress: ENTRY_FACTORY_ADDRESS,
+        entryPointAddress: ENTRY_POINT_ADDRESS,
+        validator: validator,
+        userSalt: userSaltBigInt,
       });
-      setChainID(Number(await bundler.getChainId()));
-    };
-    fetchData().catch(console.error);
-
-    const initAccount = async () => {
+      setAccountInfo({ account, isDeployed: false });
+    } catch (error) {
       try {
-        const provider = new ethers.BrowserProvider(window.ethereum);
-        const bundler = new sdk.BundlerProvider({
-          url: RPC_URL,
-          entryPoint: ENTRY_POINT_ADDRESS,
-          mode: sdk.BundlerMode.Auto,
+        const entryFactory = new AccountEntryFactory({
+          rpcUrl: bundlerRpcUrl,
+          accountEntryFactoryAddress: ENTRY_FACTORY_ADDRESS,
+          accountFactoryAddress: ACCOUNT_FACTORY_ADDRESS,
+          entryPointAddress: ENTRY_POINT_ADDRESS,
+          validator: validator,
+          userSalt: userSaltBigInt,
         });
-        const validator = new JsonRpcSignerECDSAValidator({
-          rpcUrl: RPC_URL,
-          address: ECDSA_VALIDATOR_ADDRESS,
-          owner: await provider.getSigner(),
+        const account = await imAccount.import({
+          rpcUrl: bundlerRpcUrl,
+          bundler: bundler,
+          entryAddress: await entryFactory.getAddress(),
+          validator: validator,
         });
-        let account: sdk.imAccount;
-        if (!accountInfo || !accountInfo.isDeployed) {
-          account = await sdk.imAccount.create({
-            rpcUrl: RPC_URL,
-            bundler: bundler,
-            imAccountFactoryAddress: ACCOUNT_FACTORY_ADDRESS,
-            accountEntryFactoryAddress: ENTRY_FACTORY_ADDRESS,
-            entryPointAddress: ENTRY_POINT_ADDRESS,
-            validator: validator,
-            userSalt: USER_SALT,
-          });
-          setAccountInfo({ account, isDeployed: false });
-        } else {
-          account = await sdk.imAccount.import({
-            rpcUrl: RPC_URL,
-            bundler: bundler,
-            entryAddress: accountInfo.account.getSenderAddress(),
-            validator: validator,
-          });
-        }
+        setAccountInfo({ account, isDeployed: true });
       } catch (error) {
         console.error(error);
       }
-    };
+    }
+    setInitLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData().catch(console.error);
     initAccount().catch(console.error);
   }, []);
 
@@ -94,6 +115,32 @@ const Home = () => {
   return (
     <div>
       <h1>Current Chain ID: {chainID}</h1>
+      <div>
+        <label>
+          Alchemy API URL:
+          <input
+            type="text"
+            value={bundlerRpcUrl}
+            onChange={(e) => setBundlerRpcUrl(e.target.value)}
+            placeholder="https://eth-sepolia.g.alchemy.com/v2/your-api-key"
+            required
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Salt:
+          <input
+            type="text"
+            value={userSalt}
+            onChange={(e) => setUserSalt(Number(e.target.value))}
+            required
+          />
+        </label>
+      </div>
+      <button onClick={initAccount} disabled={initLoading}>
+        {initLoading ? "Loading Account..." : "Load Account"}
+      </button>
       {accountInfo ? (
         <div>
           <p>Entry Address: {accountInfo.account.getSenderAddress()}</p>
@@ -104,7 +151,7 @@ const Home = () => {
             <button onClick={handleDeploy} disabled={loading}>
               {loading
                 ? "Deploying..."
-                : "Deploy Account (Pls deposit 0.05 ETH to your Entry Address first)"}
+                : "Deploy Account (Please deposit 0.07 ETH to your Entry Address before click this button)"}
             </button>
           )}
         </div>
